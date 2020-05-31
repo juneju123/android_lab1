47bfd259e7675ac5ca78f414fff3784382eb1e70
juju <juneju123@gmail.com>
2020-05-31 10:48:36 -0400

    step2-FINAL VERSION

--- app/src/main/java/com/example/androidlabs/MainActivity.java
+++ app/src/main/java/com/example/androidlabs/MainActivity.java
@@ -17,18 +17,15 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main_grid);
         CheckBox cb = findViewById(R.id.cb);
         String s = cb.getResources().getString(R.string.toast_message);
-        String onOff=cb.isChecked()?" on":" off";
-        boolean b = cb.isChecked();
         cb.setOnClickListener(e -> Toast.makeText(MainActivity.this, s, Toast.LENGTH_LONG).show());
-        cb.setOnCheckedChangeListener((c, isChecked) -> {
-            Snackbar.make(cb,"The switch is now"+onOff,Snackbar.LENGTH_INDEFINITE).setAction( "undo", click -> cb.setChecked(!b)).show();
+        cb.setOnCheckedChangeListener((c, b) -> {
+            String onOff= b?cb.getResources().getString(R.string.on):cb.getResources().getString(R.string.off);
+            Snackbar.make(cb,cb.getResources().getString(R.string.snackbar_msg)+onOff,Snackbar.LENGTH_LONG).setAction( cb.getResources().getString(R.string.undo), click -> cb.setChecked(!b)).show();
         });
         Switch sw = findViewById(R.id.switchBtn);
-        //  String s =cb.getResources().getString(R.string.toast_message);
-        //String onOff=sw.isChecked()?" on":" off";
-        // boolean b = sw.isChecked();
-        sw.setOnCheckedChangeListener((c, isChecked) -> {
-            Snackbar.make(cb,"The switch is now"+onOff,Snackbar.LENGTH_INDEFINITE).setAction( "undo", click -> sw.setChecked(!b)).show();
+        sw.setOnCheckedChangeListener((c, b) -> {
+            String onOff= b?sw.getResources().getString(R.string.on):sw.getResources().getString(R.string.off);
+            Snackbar.make(cb,cb.getResources().getString(R.string.snackbar_msg)+onOff,Snackbar.LENGTH_LONG).setAction( cb.getResources().getString(R.string.undo), click -> sw.setChecked(!b)).show();
         });
 
     }
