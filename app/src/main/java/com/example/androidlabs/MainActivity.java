562cd58ed95c37207147e09ca84149ac2c75be9c
juju <juneju123@gmail.com>
2020-06-01 10:52:27 -0400

    step1-Final

--- app/src/main/java/com/example/androidlabs/MainActivity.java
+++ app/src/main/java/com/example/androidlabs/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.androidlabs;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.Switch;
 import android.widget.Toast;
@@ -16,18 +17,19 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_linear);
         CheckBox cb = findViewById(R.id.cb);
+        Button btn = findViewById(R.id.button);
         String s = cb.getResources().getString(R.string.toast_message);
 
 
-        cb.setOnClickListener(e -> Toast.makeText(MainActivity.this, s, Toast.LENGTH_LONG).show());
+        btn.setOnClickListener(e -> Toast.makeText(MainActivity.this, s, Toast.LENGTH_LONG).show());
         cb.setOnCheckedChangeListener((c, b) -> {
             String onOff= b?cb.getResources().getString(R.string.on):cb.getResources().getString(R.string.off);
-            Snackbar.make(cb,cb.getResources().getString(R.string.snackbar_msg)+onOff,Snackbar.LENGTH_INDEFINITE).setAction( cb.getResources().getString(R.string.undo), click -> cb.setChecked(!b)).show();
+            Snackbar.make(cb,cb.getResources().getString(R.string.snackbar_msg)+onOff,Snackbar.LENGTH_LONG).setAction( cb.getResources().getString(R.string.undo), click -> cb.setChecked(!b)).show();
         });
         Switch sw = findViewById(R.id.switchBtn);
         sw.setOnCheckedChangeListener((c, b) -> {
            String onOff= b?sw.getResources().getString(R.string.on):sw.getResources().getString(R.string.off);
-            Snackbar.make(cb,cb.getResources().getString(R.string.snackbar_msg)+onOff,Snackbar.LENGTH_INDEFINITE).setAction( cb.getResources().getString(R.string.undo), click -> sw.setChecked(!b)).show();
+            Snackbar.make(cb,cb.getResources().getString(R.string.snackbar_msg)+onOff,Snackbar.LENGTH_LONG).setAction( cb.getResources().getString(R.string.undo), click -> sw.setChecked(!b)).show();
         });
 
 
