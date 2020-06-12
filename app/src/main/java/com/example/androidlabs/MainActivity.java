dfaa5d3bb27289a1c768a08d99dc9c2e93f46b2d
juju <juneju123@gmail.com>
2020-06-12 14:46:11 -0400

    lab3 ini commit

--- app/src/main/java/com/example/androidlabs/MainActivity.java
+++ app/src/main/java/com/example/androidlabs/MainActivity.java
@@ -2,13 +2,38 @@ package com.example.androidlabs;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
-
+    private SharedPreferences prefs = null;
+    private EditText EmailField;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        prefs = getSharedPreferences("FileName",MODE_PRIVATE);
+        String savedEmail = prefs.getString("ReserveName","");
+        EmailField = findViewById(R.id.Email_et);
+        EmailField.setText(savedEmail);
+        Button LoginBtn = findViewById(R.id.Login_button);
+        LoginBtn.setOnClickListener(bt ->{//onPause();
+            Intent goToProfile = new Intent(MainActivity.this, ProfileActivity.class);
+            goToProfile.putExtra("Email",EmailField.getText().toString());
+            startActivity(goToProfile);
+        });
+
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        SharedPreferences.Editor editor = prefs.edit();
+        editor.putString("ReserveName",EmailField.getText().toString());
+        editor.commit();
+
     }
 }
