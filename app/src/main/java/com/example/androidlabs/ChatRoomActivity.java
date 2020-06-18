0144eb39107e08e5d648a2ecc0c5a6f155edbda7
juju <juneju123@gmail.com>
2020-06-18 19:39:51 -0400

    lab4 - finish step8

--- app/src/main/java/com/example/androidlabs/ChatRoomActivity.java
+++ app/src/main/java/com/example/androidlabs/ChatRoomActivity.java
@@ -1,82 +1,121 @@
 package com.example.androidlabs;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.BaseAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.TextView;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import java.util.ArrayList;
+import java.util.List;
 
 
 public class ChatRoomActivity extends AppCompatActivity {
 
-    ArrayList<String> userInputFiled = new ArrayList<>();
-    MsgAdapter msgAdapter = new MsgAdapter();
+    private ListView msgListView;
+
+    private EditText userInput;
+
+    private Button send;
+
+    private MsgAdapter adapter;
+
+    private List<Msg> msgList = new ArrayList<Msg>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat_room);
-        EditText userInput = findViewById(R.id.userInput);
-
-
+        msgListView = (ListView)findViewById(R.id.lv) ;
+        userInput = findViewById(R.id.userInput);
+        adapter = new MsgAdapter();
+        msgListView.setAdapter(adapter);
         Button recievedBtn = findViewById(R.id.receiveBtn);
         recievedBtn.setOnClickListener(btn->{
-            userInputFiled.add(userInput.getText().toString());
-            msgAdapter.notifyDataSetChanged();
+            String content = userInput.getText().toString();
+            Msg msg = new Msg(content,true);
+            msgList.add(msg);
+           adapter.notifyDataSetChanged();
+           userInput.setText("");
 
     });
-        ListView myListView = findViewById(R.id.lv);
-        myListView.setAdapter(msgAdapter);
+        Button sendBtn = findViewById(R.id.sendBtn);
+        sendBtn.setOnClickListener(btn->{
+            String content = userInput.getText().toString();
+            Msg msg = new Msg(content,false);
+            msgList.add(msg);
+            adapter.notifyDataSetChanged();
+            userInput.setText("");
+        });
+
+
 
 
     }
+public class Msg{
+        private String content;
+        private  boolean type;
 
+        public Msg(String content,boolean type){
+            this.content = content;
+            this.type = type;
+        }
+        public String getContent(){
+            return content;
+        }
+        public boolean getType(){
+            return type;
+        }
+
+}
 
     class MsgAdapter extends BaseAdapter {
 
         @Override
         public int getCount() {
-
-            return userInputFiled.size();
+            return msgList.size();
         }
 
         @Override
-        public Object getItem(int position) {
-
-
-            return userInputFiled.get(position) ;
+        public Msg getItem(int position) {
+            return msgList.get(position);
         }
 
         @Override
         public long getItemId(int position) {
-            return (long)position;
-
+            return position;
         }
 
         @Override
         public View getView(int position, View convertView, ViewGroup parent) {
             LayoutInflater inflater = getLayoutInflater();
-            View newView = findViewById(R.id.goToChat);
-            if(newView.getId() == R.id.receiveBtn){
-            inflater.inflate(R.layout.receive_row_layout, parent, false );
-            TextView theText = newView.findViewById(R.id.receiveTV);
-            theText.setText((String)getItem(position));
-            return newView;
-        }else{
-                inflater.inflate(R.layout.send_row_layout, parent, false );
-                TextView theText = newView.findViewById(R.id.sendTV);
-                theText.setText((String)getItem(position));
-                return newView;
+            View view;
+            Msg msg = (Msg)getItem(position);
+            if(msg.getType()){//if type is true,then receive message(left side)
+                view = inflater.inflate(R.layout.receive_row_layout,parent,false);
+                TextView textView = view.findViewById(R.id.receive_msg);
+                textView.setText(((Msg) getItem(position)).getContent());
+            }else{
+                view = inflater.inflate(R.layout.send_row_layout,parent,false);
+                TextView textView = view.findViewById(R.id.send_msg);
+                textView.setText(((Msg) getItem(position)).getContent());
             }
-
+            return view;
         }
     }
-}
+    }
+
+
+
+
